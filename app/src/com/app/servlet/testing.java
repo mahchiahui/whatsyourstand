9bd57089b196b032e82ea9bcec428cf9c36c8afa
mah chia hui <mahchiahui@hotmail.sg>
2019-03-25 19:33:34 -0400

    second push

---
+++ app/src/com/app/servlet/testing.java
@@ -0,0 +1,21 @@
+package com.app.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "testing", urlPatterns = {"/testing"})
+public class testing extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher view = request.getRequestDispatcher("/html/test.html");
+        view.forward(request, response);
+    }
+}
