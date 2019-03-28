73eee16ff1979f0bd54cf79e5d9732319caa2990
Bella Yu <bellayu@BelladeMBP.wv.cc.cmu.edu>
2019-03-28 19:23:45 -0400

    add maven structure

---
+++ app/src/main/java/com/app/entity/User.java
@@ -0,0 +1,28 @@
+package com.app.entity;
+
+public class User {
+    private int userId;
+    private String username;
+    private String hashedPwd;
+
+    public User(int userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
